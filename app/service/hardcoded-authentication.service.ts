1d33f1f11a7e9fd4c7a7884fc201ff92a4edafcd
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-08-01 15:52:39 +0200

    35. Using session storage to store User Authentication

--- app/service/hardcoded-authentication.service.ts
+++ app/service/hardcoded-authentication.service.ts
@@ -8,9 +8,17 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username: any, password: any) {
+    console.log('before' + this.isUserLoggedIn());
     if(username === "kash" && password === 'dummy'){
+      sessionStorage.setItem('authenticaterUser', username)
+      console.log('after' + this.isUserLoggedIn());
       return true;
     } 
       return false;
+    }
+
+    isUserLoggedIn(){
+      let user = sessionStorage.getItem('authenticaterUser')
+      return !(user === null)
     }
   }
